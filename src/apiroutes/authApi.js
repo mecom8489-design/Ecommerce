19d758aa229eaeb7fe82b3e7ce81ad3d8ce146ac
Iyyanar15 <iyyanaraahasolutions@gmail.com>
2025-09-08 17:03:35 +0530

    sampcheck

--- src/apiroutes/authApi.js
+++ src/apiroutes/authApi.js
@@ -12,3 +12,5 @@ export const GetSlides = () =>
   axiosInstance.get("/slides/get");
 export const ContactEmail = () =>
   axiosInstance.get("/contact/email");
+export const About = () =>
+  axiosInstance.get("/contact/about");
