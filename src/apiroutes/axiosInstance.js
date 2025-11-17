584d85810a69f5e2f2f7d096397bb1cb127bc7e2
mecom8489 <mecom8489@gmail.com>
2025-11-17 16:25:10 +0530

    baseURL: "https://e-commerce-backend-production-6fa0.up.railway.app/api",

--- src/apiroutes/axiosInstance.js
+++ src/apiroutes/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const axiosInstance = axios.create({
-  baseURL: "https://e-commerce-backend-by70.onrender.com/api",
+  baseURL: "https://e-commerce-backend-production-6fa0.up.railway.app/api",
 });
 
 // Optional: Add interceptors for token auth
