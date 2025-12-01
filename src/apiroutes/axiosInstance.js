81411c0c598a60e8ba035edf2bc6455b46c341dd
mecom8489 <mecom8489@gmail.com>
2025-12-01 10:50:59 +0530

    APi changed

--- src/apiroutes/axiosInstance.js
+++ src/apiroutes/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const axiosInstance = axios.create({
-  baseURL: "https://e-commerce-backend-production-6fa0.up.railway.app/api",
+  baseURL: "e-commerce-backend-rho-rouge.vercel.app/api",
 });
 
 // Optional: Add interceptors for token auth
