e4a60f11ba6fbe22d729cb78dd8a2ae79032f5e3
mecom8489 <mecom8489@gmail.com>
2025-12-10 10:45:33 +0530

    baseURL: "https://e-commerce-backend-rho-rouge.vercel.app/api",

--- src/apiroutes/axiosInstance.js
+++ src/apiroutes/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://e-commerce-backend-rho-rouge.vercel.app/api",
 });
 
 // Optional: Add interceptors for token auth
